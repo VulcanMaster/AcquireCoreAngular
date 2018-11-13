8587bfe80d0d24f8789b51a2be1b361f49e2c523
Dmitrijs Blinkovs <dmitry.blinkov@outlook.com>
2018-11-13 11:59:43 +0200

    Added support for jQuery. Some small css change.

--- AcqureCoreAng/AcqureCoreAng.Web/Startup.cs
+++ AcqureCoreAng/AcqureCoreAng.Web/Startup.cs
@@ -22,6 +22,7 @@ namespace AcqureCoreAng.Web
         {
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseNodeModules(env);
             
 
             //if (env.IsDevelopment())
---
+++ AcqureCoreAng/AcqureCoreAng.Web/wwwroot/js/index.js
@@ -0,0 +1,20 @@
+﻿var z = 666;
+
+console.log(z);
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+document.getElementById("buyButton").addEventListener("click", alertMe);
+
+function alertMe() {
+    alert("Buying item.")
+    var theForm = document.getElementById("theForm");
+    theForm.hidden = false;
+}
+
+//var button = document.getElementById("buyButton");
+
+//button.addEventListener("click", function() {
+//    alert("Buying item");
+//});
