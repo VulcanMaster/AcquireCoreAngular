dc94a1e0950900008cd21ef94d68bca763b23cd2
Dmitrijs Blinkovs <dmitry.blinkov@outlook.com>
2018-11-13 14:26:04 +0200

    One more form for login, the jquery toggle, some css.

--- AcqureCoreAng/AcqureCoreAng.Web/wwwroot/js/index.js
+++ AcqureCoreAng/AcqureCoreAng.Web/wwwroot/js/index.js
@@ -1,31 +1,39 @@
-﻿var z = 666;
+﻿$(document).ready(function () {
 
-console.log(z);
 
-var theForm = $("#theForm");
-theForm.hide();
-//theForm.hidden = true;
+    var z = 666;
 
-$("#buyButton").on("click", alertMe);
+    console.log(z);
 
-function alertMe() {
-    alert("Buying Item.")
-    var theForm = document.getElementById("theForm");
-    theForm.hidden = false;
-}
+    var theForm = $("#theForm");
+    theForm.hide();
+    //theForm.hidden = true;
 
-var productInfo = $(".product-props li")
+    $("#buyButton").on("click", alertMe);
 
-productInfo.on("click", function() {
-    console.log("You clicked on " + $(this).text());
-})
+    function alertMe() {
+        alert("Buying Item.")
+        var theForm = document.getElementById("theForm");
+        theForm.hidden = false;
+    }
 
-debugger;
+    var productInfo = $(".product-props li")
+
+    productInfo.on("click",
+        function () {
+            console.log("You clicked on " + $(this).text());
+        });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function() {
+        $popupForm.fadeToggle(1000);
+    })
+});
+
 
 //var listItems = productInfo.item[0].children;
-
-debugger;
-
 //document.getElementById("buyButton").addEventListener("click", alertMe);
 //function alertMe() {
 //    alert("Buying item.")
