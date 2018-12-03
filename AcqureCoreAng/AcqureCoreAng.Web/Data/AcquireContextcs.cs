796f18a1024245560bd25e051b36bd284cb08283
Dmitrijs Blinkovs <dmitry.blinkov@outlook.com>
2018-12-03 11:41:20 +0200

    Entitty Framework, migrations

---
+++ AcqureCoreAng/AcqureCoreAng.Web/Data/AcquireContextcs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AcqureCoreAng.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcqureCoreAng.Web.Data
+{
+    public class AcquireContext : DbContext    {
+        public AcquireContext(DbContextOptions<AcquireContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
--- AcqureCoreAng/AcqureCoreAng.Web/Startup.cs
+++ AcqureCoreAng/AcqureCoreAng.Web/Startup.cs
@@ -2,20 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AcqureCoreAng.Web.Data;
 using AcqureCoreAng.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AcqureCoreAng.Web
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            this._config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<AcquireContext>(cfg =>
+            //{
+            //    _config.GetConnectionString("AcquireConnectionString");
+            //    //cfg.UseSqlServer("");
+            //});
+
+
+            //Tell services that we'd like it to use our context.
+            services.AddDbContext<AcquireContext>(cfg =>
+            {
+
+                cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
+
+
+                //if (_env.IsDevelopment())
+                //{
+                //    cfg.UseSqlite("Data Source=DutchDb");
+                //}
+
+                //if (_env.IsProduction())
+                //{
+                //    //Get the connection string from configuration.
+                    
+                //}
+            });
+
             services.AddTransient<IMailService, NullMailService>();
 
             // add suppiort for real email service
